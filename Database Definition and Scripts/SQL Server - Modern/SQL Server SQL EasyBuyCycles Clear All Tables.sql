a054a49386d34f0a205122dd55f49abd1c062c0e
Lee Paul <lpaul@surroundtech.com>
2024-06-05 23:29:22 -0400

    Database Definition and Scripts: Corrections for Modern version of Database Name. Removed the word Modern.

---
+++ Database Definition and Scripts/SQL Server - Modern/SQL Server SQL EasyBuyCycles Clear All Tables.sql
@@ -0,0 +1,12 @@
+-- Be careful! This is a dangerous script!
+-- Uncomment the commands you need to run.
+
+-- USE "EasyBuyCycles";
+-- USE "EasyBuyCyclesDev";
+-- USE "EasyBuyCyclesModern";
+
+-- DELETE FROM [dbo].[OrderItem]
+-- DELETE FROM [dbo].[Order]
+-- DELETE FROM [dbo].[Product]
+-- DELETE FROM [dbo].[ShippingAddress]
+-- DELETE FROM [dbo].[Customer]
