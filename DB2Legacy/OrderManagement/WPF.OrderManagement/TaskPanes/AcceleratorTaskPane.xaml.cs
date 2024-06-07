d5c7e1291bc1d76f2f6b78aa356912dfa1da79e5
Devin Paul <36082215+DevinPaul@users.noreply.github.com>
2024-06-07 14:59:46 -0500

    Added Task Panes
    * Added Task Panes
    
    * Updated Accelerator Task Pane URL

---
+++ DB2Legacy/OrderManagement/WPF.OrderManagement/TaskPanes/AcceleratorTaskPane.xaml.cs
@@ -0,0 +1,33 @@
+﻿using A4DN.Core.WPF.Base;
+using BOS.OrderManagement.Shared.Properties;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF.OrderManagement.TaskPanes
+{
+	public partial class AcceleratorTaskPane : AB_TaskPanePresenter
+	{
+		public AcceleratorTaskPane()
+		{
+			InitializeComponent();
+		}
+
+		protected override void am_SetParentProperties()
+		{
+			base.am_SetParentProperties();
+			ap_Title = "Accelerator";
+			ap_PreferredHeightIfDockedToTopOrBottom = 140;
+			ap_PreferredWidthIfDockedToLeftOrRight = 300;
+			ap_ImageFileName = "AcceleratorIcon.png";
+		}
+
+		private void _OpenUrl()
+		{
+			string url = "https://www.surroundtech.com/accelerator";
+			System.Diagnostics.Process.Start(url);
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e) => _OpenUrl();
+		private void Image_PreviewMouseDown(object sender, MouseButtonEventArgs e) => _OpenUrl();
+	}
+}
---
+++ DB2Legacy/OrderManagement/WPF.OrderManagement/TaskPanes/SurroundTaskPane.xaml.cs
@@ -0,0 +1,49 @@
+﻿using A4DN.Core.WPF.Base;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF.OrderManagement.TaskPanes
+{
+	/// <summary>
+	/// Interaction logic for SurroundTaskPane.xaml
+	/// </summary>
+	public partial class SurroundTaskPane : AB_TaskPanePresenter
+	{
+		public SurroundTaskPane()
+		{
+			InitializeComponent();
+		}
+
+		protected override void am_SetParentProperties()
+		{
+			base.am_SetParentProperties();
+			ap_Title = "Surround";
+			ap_PreferredHeightIfDockedToTopOrBottom = 120;
+			ap_PreferredWidthIfDockedToLeftOrRight = 300;
+			ap_ImageFileName = "SurroundTask.png";
+		}
+
+		private void TextBlock_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			var textBlock = sender as TextBlock;
+			var text = textBlock.Text;
+			if (text.Contains('@'))
+			{
+				// Email Address prefex with mailto
+				text = "mailto:" + text;
+			}
+			else
+			{
+				text = "http://" + text;
+			}
+
+			_OpenUrl(text);
+		}
+
+		private void _OpenUrl(string Url)
+		{
+			System.Diagnostics.Process.Start(Url);
+		}
+	}
+}
