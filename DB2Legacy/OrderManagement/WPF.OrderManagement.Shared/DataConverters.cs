034f2f9e8816a7d0cd34d9b0942fa9f21b1e4786
jason-gardiner <119627004+jason-gardiner@users.noreply.github.com>
2024-06-18 11:19:39 -0400

    Enhance logon icon/font style (#20)
    * Changes to Customer Detail
    
    * Updated Order Item Detail
    
    * Removed Unnecessary Comments
    
    * Updated Order Detail
    
    * Removed Blue Line Brush from Product Detail and ApplicationResourceDictionary
    
    * Updated Shipping Address Detail
    
    * Reformatted xaml code for Product and Customer Detail
    
    * Updated the size of the Product and Customer detail pop out windows to fit the size of the content
    
    * Enhanced the Logon Screen and added Brushes  for the primary, secondary, and tertiary colors in the logo
    
    ---------
    
    Co-authored-by: jason-gardiner <jgardiner@surroundtech.com>

--- DB2Legacy/OrderManagement/WPF.OrderManagement.Shared/DataConverters.cs
+++ DB2Legacy/OrderManagement/WPF.OrderManagement.Shared/DataConverters.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows;
+using A4DN.Core.BOS.Base;
 
 namespace WPF.OrderManagement.Shared
 {
@@ -13,6 +14,29 @@ namespace WPF.OrderManagement.Shared
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if (value is int _int && _int != 0) return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NullCheckToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string _str && !string.IsNullOrWhiteSpace(_str)) return Visibility.Visible;
+
+            if (value is int) return Visibility.Visible;
+
+            if (value is decimal) return Visibility.Visible;
+
+            if (value is DateTime) return Visibility.Visible;
+
+            if (value is AB_BusinessObjectEntityBase _ent && _ent != null) return Visibility.Visible;
 
             return Visibility.Collapsed;
         }
