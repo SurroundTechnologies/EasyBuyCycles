dab41d1a88bce4ad65aa5ee45319cfe485e7a8a9
Lee Paul <44013679+SurroundLeePaul@users.noreply.github.com>
2024-06-20 08:55:45 -0400

    SQL and DB2 Source: Updated Views with additional values

--- Database Definition and Scripts/DB2 for i - Modern/IBM i SQL EasyBuyCycles Modern VW_OrderAllDetails.sql
+++ Database Definition and Scripts/DB2 for i - Modern/IBM i SQL EasyBuyCycles Modern VW_OrderAllDetails.sql
@@ -44,57 +44,57 @@ CREATE OR REPLACE VIEW "VW_Order_AllDetails"
     FOR SYSTEM NAME "YD1OV01" AS
     
     WITH "OrderItems" AS (
-    	SELECT "OrderInternalID"
+        SELECT "OrderInternalID"
                 ,COUNT(*) AS "OrderItemCount"
-    			,DECIMAL(SUM("Quantity" * "UnitPrice"),11,2) AS "OrderTotal"
-    			,DECIMAL(SUM("Quantity" * "UnitPrice" * ("DiscountPercent" * 0.01)),11,2) AS "OrderDiscount"
-    			,DECIMAL(SUM("Quantity" * "UnitPrice" * (1 - ("DiscountPercent" * 0.01))),11,2) AS "OrderDiscountedTotal"
-    		FROM "OrderItem"
-    		GROUP BY "OrderInternalID"
+                ,DECIMAL(SUM("Quantity" * "UnitPrice"),11,2) AS "OrderTotal"
+                ,DECIMAL(SUM("Quantity" * "UnitPrice" * ("DiscountPercent" * 0.01)),11,2) AS "OrderDiscount"
+                ,DECIMAL(SUM("Quantity" * "UnitPrice" * (1 - ("DiscountPercent" * 0.01))),11,2) AS "OrderDiscountedTotal"
+            FROM "OrderItem"
+            GROUP BY "OrderInternalID"
     )
     SELECT "Order"."InternalID"
-    		,"Order"."CustomerInternalID"
-    		,"Order"."OrderDateTime"
-    		,"Order"."PurchaseOrderNumber"
-    		,"Order"."WarehouseInternalID"
-    		,"Order"."WarehouseName"
-    		,"Order"."DeliveryMemo"
-    		,"Order"."ShippingAddressInternalID"
-    		,"Order"."OrderMemo"
-    		,"Order"."Status"
-    		,"Order"."SalesPersonInternalID"
-    		,"Order"."SalesPersonName"
-    		,"Order"."PurchasePoints"
+            ,"Order"."CustomerInternalID"
+            ,"Order"."OrderDateTime"
+            ,"Order"."PurchaseOrderNumber"
+            ,"Order"."WarehouseInternalID"
+            ,"Order"."WarehouseName"
+            ,"Order"."DeliveryMemo"
+            ,"Order"."ShippingAddressInternalID"
+            ,"Order"."OrderMemo"
+            ,"Order"."Status"
+            ,"Order"."SalesPersonInternalID"
+            ,"Order"."SalesPersonName"
+            ,"Order"."PurchasePoints"
     
     -- Joins to Customer
-    		,"Customer"."Name" AS "CustomerName"
+            ,"Customer"."Name" AS "CustomerName"
             ,"Customer"."BillingAddress1"
-    		   CONCAT CASE "Customer"."BillingAddress2" WHEN '' THEN '' ELSE ', ' CONCAT "Customer"."BillingAddress2" END
-    		   CONCAT CASE "Customer"."BillingAddress3" WHEN '' THEN '' ELSE ', ' CONCAT "Customer"."BillingAddress3" END
-    		   CONCAT CASE "Customer"."BillingPostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "Customer"."BillingPostalCode" END
-    		   CONCAT CASE "Customer"."BillingCountry" WHEN '' THEN '' ELSE ', ' CONCAT "Customer"."BillingCountry" END
-    		   AS "CustomerBillingAddressLine"
-    		,"Customer"."BillingAddress1"
-    		   CONCAT CASE "Customer"."BillingAddress2" WHEN '' THEN '' ELSE CHR(13) CONCAT "Customer"."BillingAddress2" END
-    		   CONCAT CASE "Customer"."BillingAddress3" WHEN '' THEN '' ELSE CHR(13) CONCAT "Customer"."BillingAddress3" END
-    		   CONCAT CASE "Customer"."BillingPostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "Customer"."BillingPostalCode" END
-    		   CONCAT CASE "Customer"."BillingCountry" WHEN '' THEN '' ELSE CHR(13) CONCAT "Customer"."BillingCountry" END
-    		   AS "CustomerBillingAddressBlock"    
+               CONCAT CASE "Customer"."BillingAddress2" WHEN '' THEN '' ELSE ', ' CONCAT "Customer"."BillingAddress2" END
+               CONCAT CASE "Customer"."BillingAddress3" WHEN '' THEN '' ELSE ', ' CONCAT "Customer"."BillingAddress3" END
+               CONCAT CASE "Customer"."BillingPostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "Customer"."BillingPostalCode" END
+               CONCAT CASE "Customer"."BillingCountry" WHEN '' THEN '' ELSE ', ' CONCAT "Customer"."BillingCountry" END
+               AS "CustomerBillingAddressLine"
+            ,"Customer"."BillingAddress1"
+               CONCAT CASE "Customer"."BillingAddress2" WHEN '' THEN '' ELSE CHR(13) CONCAT "Customer"."BillingAddress2" END
+               CONCAT CASE "Customer"."BillingAddress3" WHEN '' THEN '' ELSE CHR(13) CONCAT "Customer"."BillingAddress3" END
+               CONCAT CASE "Customer"."BillingPostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "Customer"."BillingPostalCode" END
+               CONCAT CASE "Customer"."BillingCountry" WHEN '' THEN '' ELSE CHR(13) CONCAT "Customer"."BillingCountry" END
+               AS "CustomerBillingAddressBlock"    
     
     -- Joins to ShippingAddress
             ,"ShippingAddress"."Name" AS "ShippingAddressName"
             ,"ShippingAddress"."Address1"
-    		   CONCAT CASE "ShippingAddress"."Address2" WHEN '' THEN '' ELSE ', ' CONCAT "ShippingAddress"."Address2" END
-    		   CONCAT CASE "ShippingAddress"."Address3" WHEN '' THEN '' ELSE ', ' CONCAT "ShippingAddress"."Address3" END
-    		   CONCAT CASE "ShippingAddress"."PostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "ShippingAddress"."PostalCode" END
-    		   CONCAT CASE "ShippingAddress"."Country" WHEN '' THEN '' ELSE ', ' CONCAT "ShippingAddress"."Country" END
-    		   AS "ShippingAddressLine"
-    		,"ShippingAddress"."Address1"
-    		   CONCAT CASE "ShippingAddress"."Address2" WHEN '' THEN '' ELSE CHR(13) CONCAT "ShippingAddress"."Address2" END
-    		   CONCAT CASE "ShippingAddress"."Address3" WHEN '' THEN '' ELSE CHR(13) CONCAT "ShippingAddress"."Address3" END
-    		   CONCAT CASE "ShippingAddress"."PostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "ShippingAddress"."PostalCode" END
-    		   CONCAT CASE "ShippingAddress"."Country" WHEN '' THEN '' ELSE CHR(13) CONCAT "ShippingAddress"."Country" END
-    		   AS "ShippingAddressBlock"   
+               CONCAT CASE "ShippingAddress"."Address2" WHEN '' THEN '' ELSE ', ' CONCAT "ShippingAddress"."Address2" END
+               CONCAT CASE "ShippingAddress"."Address3" WHEN '' THEN '' ELSE ', ' CONCAT "ShippingAddress"."Address3" END
+               CONCAT CASE "ShippingAddress"."PostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "ShippingAddress"."PostalCode" END
+               CONCAT CASE "ShippingAddress"."Country" WHEN '' THEN '' ELSE ', ' CONCAT "ShippingAddress"."Country" END
+               AS "ShippingAddressLine"
+            ,"ShippingAddress"."Address1"
+               CONCAT CASE "ShippingAddress"."Address2" WHEN '' THEN '' ELSE CHR(13) CONCAT "ShippingAddress"."Address2" END
+               CONCAT CASE "ShippingAddress"."Address3" WHEN '' THEN '' ELSE CHR(13) CONCAT "ShippingAddress"."Address3" END
+               CONCAT CASE "ShippingAddress"."PostalCode" WHEN '' THEN '' ELSE SPACE(1) CONCAT "ShippingAddress"."PostalCode" END
+               CONCAT CASE "ShippingAddress"."Country" WHEN '' THEN '' ELSE CHR(13) CONCAT "ShippingAddress"."Country" END
+               AS "ShippingAddressBlock"   
     
     -- Audit Stamps
             ,"Order"."CreatedAt" AS "CreatedAt"
@@ -104,10 +104,10 @@ CREATE OR REPLACE VIEW "VW_Order_AllDetails"
             ,"Order"."LastModifiedBy" AS "ModifiedBy"
             ,"Order"."LastModifiedWith" AS "ModifiedWith"
     
-    	FROM "Order"
-    	LEFT JOIN "OrderItems" ON "OrderItems"."OrderInternalID" = "Order"."InternalID"
-        LEFT JOIN "Customer" ON "Customer"."InternalID" = "Order"."CustomerInternalID"
-        LEFT JOIN "ShippingAddress" ON "ShippingAddress"."InternalID" = "Order"."ShippingAddressInternalID"
+        FROM "Order"
+            LEFT JOIN "OrderItems" ON "OrderItems"."OrderInternalID" = "Order"."InternalID"
+            LEFT JOIN "Customer" ON "Customer"."InternalID" = "Order"."CustomerInternalID"
+            LEFT JOIN "ShippingAddress" ON "ShippingAddress"."InternalID" = "Order"."ShippingAddressInternalID"
 ;
 
 -- View Description
@@ -116,12 +116,12 @@ LABEL ON TABLE "VW_Order_AllDetails"
 
 -- Column Labels - Description (50 Chars)
 LABEL ON COLUMN "VW_Order_AllDetails" (
-     "CustomerName"              TEXT IS 'Customer Name'
-     ,"CustomerBillingAddressLine"          TEXT IS 'Customer Billing Address Line'
-     ,"CustomerBillingAddressBlock"         TEXT IS 'Customer Billing Address Block'
-     ,"ShippingAddressName"              TEXT IS 'Shipping Address Name'
-     ,"ShippingAddressLine"          TEXT IS 'Shipping Address Line'
-     ,"ShippingAddressBlock"          TEXT IS 'Shipping Address Block'
+     "CustomerName"                 TEXT IS 'Customer Name'
+     ,"CustomerBillingAddressLine"  TEXT IS 'Customer Billing Address Line'
+     ,"CustomerBillingAddressBlock" TEXT IS 'Customer Billing Address Block'
+     ,"ShippingAddressName"         TEXT IS 'Shipping Address Name'
+     ,"ShippingAddressLine"         TEXT IS 'Shipping Address Line'
+     ,"ShippingAddressBlock"        TEXT IS 'Shipping Address Block'
 );
 
 -- Grant View Access
