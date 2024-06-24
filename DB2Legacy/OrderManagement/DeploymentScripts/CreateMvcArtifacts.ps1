f14c47df2367373031f728b8333716223c54d102
Patrick Davis <patrick@thepatrickdavis.com>
2024-06-23 21:07:49 -0400

    Add MVC deployment capabilities

--- DB2Legacy/OrderManagement/DeploymentScripts/CreateMvcArtifacts.ps1
+++ DB2Legacy/OrderManagement/DeploymentScripts/CreateMvcArtifacts.ps1
@@ -1,6 +1,7 @@
 # If you get an error running this script run the following command:
 # > Set-ExecutionPolicy -Scope CurrentUser unrestricted
 
+param($ApplicationVersion)
 # Script is configured to build with full symbos for debuging both release and debug folders
 function BuildSolution    
 {
@@ -15,13 +16,19 @@ function BuildSolution
 
 $msbuild = (&"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe)
 $VisualStudioVersion = "17.0"
-$ToolsVersion = "Current"
+$ToolsVersion = "17.0"
 $CommitHash = (git rev-parse HEAD).Substring(0,16)
-$location = Get-Location
+$ApplicationVersion="1.0.0.3"
+$PublisherName="EasyBuy Cycles Distributors"
+$SolutionSystemName="OrderManagement"
+$MagePath = "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\mage.exe"
+$SevenZipPath="C:\Program Files\7-Zip\7z.exe"
+$MvcLocation="..\MVC.OrderManagement\MVC.OrderManagement.csproj"
+$SolutionFolder="C:\Surround\EasyBuyCycles\DB2Legacy\OrderManagement"
 
 Write-Output "========================================================================"
 Write-Output "=                                                                      ="
-Write-Output "= ALERT: This is a dev build only do not use to deploy Production      ="
+Write-Output "= ALERT: This is a dev artifact only do not use to deploy Production      ="
 Write-Output "=                                                                      ="
 Write-Output "========================================================================"
 Write-Output " "
@@ -30,37 +37,21 @@ Write-Output "Visual Studio Version: $VisualStudioVersion"
 Write-Output "ToolsVersion: $ToolsVersion"
 Write-Output "Commit Hash: $CommitHash"
 
-$MSBuildArgs = @(
-    "/maxcpucount",
-    "/p:Configuration=Release",
-    "/p:DebugSymbols=true",
-    "/p:DebugType=Full",
-    "/p:Platform=Any CPU",
-    "/p:AllowUnsafeBlocks=true",
-    "/nodeReuse:false",
+$PublishArgs = @(
+    # "/p:EnvID=$EnvID1Name",
+    # "/p:DeploymentFolder=$DeploymentFolder",
+    "/p:SolutionSystemName=$SolutionSystemName",
+    "/p:SolutionFolder=$SolutionFolder",
+    "/p:PublisherName=$PublisherName"
+    "/p:ApplicationVersion=$ApplicationVersion",
     "/consoleloggerparameters:ErrorsOnly;ShowTimeStamp",
-    "/p:VisualStudioVersion=$VisualStudioVersion",
+    "/maxcpucount",
     "/p:ToolsVersion=$ToolsVersion",
-    "/p:SourceRevisionId=$CommitHash"
-
+    "/p:DeployOnBuild=true",
+    "/p:PublishProfile=FolderProfile"
 );
 
-$MSBuildArgsDebug = @(
-    "/maxcpucount",
-    "/p:Configuration=Debug",
-    "/p:DebugSymbols=true",
-    "/p:DebugType=Full",
-    "/p:Platform=Any CPU",
-    "/p:AllowUnsafeBlocks=true",
-    "/nodeReuse:false",
-    "/consoleloggerparameters:ErrorsOnly;ShowTimeStamp",
-    "/p:VisualStudioVersion=$VisualStudioVersion",
-    "/p:ToolsVersion=$ToolsVersion"
-);
+Write-Output "Publishing MVC"
+& $msbuild (@($MvcLocation) + $PublishArgs)
 
-Write-Output "Cleaning up bin and obj folders"
-Get-ChildItem .. -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
-
-BuildSolution $msbuild "..\OrderManagement.sln" $MSBuildArgs
-
-Write-Output "Build Complete"
+Write-Output "MVC published successfully"
