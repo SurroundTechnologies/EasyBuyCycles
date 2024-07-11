dede2d3f710766a337635aed04412dc9d6b2b997
jason-gardiner <jgardiner@surroundtech.com>
2024-07-11 12:58:52 -0400

    Fixed Issues with the Address Controls, and Made the Maps Button Red

--- DB2Legacy/OrderManagement/WPF.OrderManagement.Shared/AddressControls.xaml.cs
+++ DB2Legacy/OrderManagement/WPF.OrderManagement.Shared/AddressControls.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using A4DN.Core.WPF.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WPF.OrderManagement.Shared;
+
 
 namespace WPF.OrderManagement.Shared
 {
@@ -36,7 +24,7 @@ namespace WPF.OrderManagement.Shared
             get { return (string)GetValue(FullAddressBlockProperty); }
             set { SetValue(FullAddressBlockProperty, value); }
         }
-        public static readonly DependencyProperty FullAddressBlockProperty = DependencyProperty.Register("FullAddressBlockBlock", typeof(string), typeof(AddressControls), new PropertyMetadata(null));
+        public static readonly DependencyProperty FullAddressBlockProperty = DependencyProperty.Register("FullAddressBlock", typeof(string), typeof(AddressControls), new PropertyMetadata(null));
 
         public AB_MessageConsole MessageConsole
         {
